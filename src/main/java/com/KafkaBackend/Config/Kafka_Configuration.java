b40b7f0215187e4ac5b47276708745b61d6677f8
SavinashSinnasamy <savinash.20210847@iit.ac.lk>
2024-03-19 13:35:33 +0530

    updated docker file

--- src/main/java/com/KafkaBackend/Config/Kafka_Configuration.java
+++ src/main/java/com/KafkaBackend/Config/Kafka_Configuration.java
@@ -10,7 +10,6 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.mapping.DefaultJackson2JavaTypeMapper;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
@@ -29,7 +28,7 @@ public class Kafka_Configuration {
     @Bean
     public ConsumerFactory<String, FormObject> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka-broker-container:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "requestingBanks");
         // other consumer properties
         JsonDeserializer<FormObject> deserializer = new JsonDeserializer<>(FormObject.class);
